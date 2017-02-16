8b45bd8a5ce1028911c5f149b79ca71d4121d3e2
Justen DePourcq <jdepourcq3@my.bcit.ca>
2017-02-16 14:52:28 -0800

    Created models for each of the database tables

---
+++ application/models/Flags.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Flags
+ *
+ * @author JDePourcq
+ */
+class Flags extends MY_Model {
+
+        public function __construct()
+        {
+                parent::__construct('flags', 'id');
+        }
+}
---
+++ application/models/Groups.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Group
+ *
+ * @author JDePourcq
+ */
+class Groups extends MY_Model {
+
+        public function __construct()
+        {
+                parent::__construct('groups', 'id');
+        }
+}
